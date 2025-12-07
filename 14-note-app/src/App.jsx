dbd6ec5badb994f9318df8438136fb1c66664634
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-07 13:18:58 +0545

    Note App has done now

--- 14-note-app/src/App.jsx
+++ 14-note-app/src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 // newNote - string "Do Homework"
 // {id, text, completed}
+// const fruits = ['apple', 'banana', 'mango', 'orange'];
+// console.log(fruits.includes('apple'))
+
+// const word = "aeroplane"
+// console.log(word.includes('ae'))
 const App = () => {
   // states
   const [newNote, setNewNote] = useState("");
   const [notes, setNotes] = useState([]);
   const [editingId, setEditingId] = useState(null);
   const [editingText, setEditingText] = useState("")
+  const [search, setSearch] = useState("");
 
   const handleChange = (e) => {
     setNewNote(e.target.value);
@@ -35,10 +41,30 @@ const handleEdit = (id, text) => {
 }
 
 // cancelEdit
-const handleEditCancel = (id) => {
+const handleEditCancel = () => {
   setEditingId(null);
   setEditingText("")
 }
+
+// Save Edit
+const handleSaveEdit = (id) => {
+  const updatedNote = notes.map(note => id === note.id ? {...note, text: editingText} : note)
+  setNotes(updatedNote)
+  setEditingId(null)
+  setEditingText("")
+}
+
+// Search
+const filteredNote = notes.filter((note) => (
+  note.text.toLowerCase().includes(search.toLowerCase())
+))
+
+// console.log(search)
+
+
+// Render elements
+// console.log(editingText)
+
 
 
 
@@ -47,17 +73,17 @@ const handleEditCancel = (id) => {
       <h1>Note App</h1>
       <input value={newNote} onChange={handleChange} type="text" placeholder="Enter the note" />
       <button onClick={handleAddNote}>Add</button>
-      <input type="text" placeholder="Search note" />
+      <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Search note" />
       <button>Search</button>
       
       <ul>
-        {notes.map((note) => (
+        {filteredNote.map((note) => (
           <li key={note.id}>
           {editingId === note.id ? (
             <div>
-              <input value={editingText} type="text"/>
-              <button>Save</button>
-              <button onClick={() => handleEditCancel(note.id)}>Cancel</button>
+              <input onChange={(e => setEditingText(e.target.value))} value={editingText} type="text"/>
+              <button onClick={() => handleSaveEdit(note.id)}>Save</button>
+              <button onClick={handleEditCancel}>Cancel</button>
             </div>
             
           ) : (
