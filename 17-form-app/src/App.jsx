b31485f888a922139fd78f251f94fbbf02d18ca8
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-11 22:03:56 +0545

    Adding React Icons

---
+++ 17-form-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaFacebookMessenger } from "react-icons/fa6";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Messegers</h1>
+      <FaFacebookMessenger size={100} color='blue'/>
+    </div>
+  )
+}
+
+export default App
