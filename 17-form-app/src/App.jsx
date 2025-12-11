897f09d11b971b951169c86f9d0b62cab438416e
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-11 22:32:34 +0545

    Added Form Handling

--- 17-form-app/src/App.jsx
+++ 17-form-app/src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
+import { FaHome } from "react-icons/fa";
 import { FaFacebookMessenger } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
+import { IoIosContact } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const App = () => {
   return (
     <div>
       <h1>Messegers</h1>
-      <FaFacebookMessenger size={100} color='blue'/>
+      <FaHome size={100} color="blue" />
+      <FaFacebookMessenger size={100} color="blue" />
+      <IoMail size={100} color="blue" />
+      <IoIosContact size={100} color="blue" />
+      <FaInstagram size={100} color="blue"/>
+      <FaLinkedin size={100} color="blue"/>
+      <FaGithub size={100} color="blue"/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
