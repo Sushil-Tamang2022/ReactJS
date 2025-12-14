b35a53fb5fd37a1111eb4c46fbe1e6c5cab435ab
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-14 13:17:12 +0545

    Finished useref app

---
+++ 18-useref-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1 className='bg-amber-200 sm:bg-blue-600 md:bg-green-400 lg:bg-pink-300'>Tailwind css</h1>
+    </div>
+  )
+}
+
+export default App
